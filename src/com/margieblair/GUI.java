b97ca97a01daa6660cc627d11d0d0537c42c7be3
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-23 13:05:45 -0400

    Initial Commit

---
+++ src/com/margieblair/GUI.java
@@ -0,0 +1,15 @@
+package com.margieblair;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+    }
+}
